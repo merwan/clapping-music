79d59df1cac2b2646406e51c251a20a71aa4ddfd
Merouane Atig <merwan@patem.net>
2016-08-31 00:22:57 +0200

    Play a clap sample

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clapping Music
+
+Clapping hands sample from http://free-loops.com/6795-hand-clap.html
--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 window.addEventListener('load', function() {
-  console.log('loaded');
+  var context = new AudioContext();
+
+  function loadSample(sample) {
+    var request  = new XMLHttpRequest();
+    request.responseType = 'arraybuffer';
+    request.addEventListener('load', decodeAudio);
+    request.open('GET', sample);
+    request.send();
+  }
+
+  function decodeAudio() {
+    context.decodeAudioData(this.response, decodeBuffer, errorFun);
+  }
+
+  function decodeBuffer(decodedBuffer) {
+    playBuffer(decodedBuffer);
+  }
+
+  function errorFun(error) {
+    console.error('Cannot decode buffer', error);
+  }
+
+  function playBuffer(buffer) {
+    source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
+  }
+
+  loadSample('clap.wav');
 });
